8608149a2b70d8498d4f2d6263912cb07433dd2a
qnd303 <doqn@dukes.jmu.edu>
2025-11-19 09:40:21 -0500

    exactp2

--- exact_solution/exact_solution.py
+++ exact_solution/exact_solution.py
@@ -7,33 +7,23 @@ Comments here on your code and submission.
 Citations: https://www.geeksforgeeks.org/dsa/introduction-and-approximate-solution-for-vertex-cover-problem/
 """
 
-def exact_vertex_cover(graph, V):
-    best = None
-    
-    def dfs(i, cover):
-        nonlocal best
+from itertools import combinations
 
-        # prune
-        if best is not None and len(cover) >= len(best):
-            return
-        
-        # if checked all vertices
-        if i == V:
-            # check if cover covers all edges
+def brute_force_vertex_cover(graph, V):
+    for k in range(V + 1):
+        for subset in combinations(range(V), k):
+            cover = set(subset)
+
+            valid = True
             for u in range(V):
                 for v in graph[u]:
                     if u not in cover and v not in cover:
-                        return
-            best = cover.copy()
-            return
-        
-        # Case 1: don't include i
-        dfs(i + 1, cover)
-        
-        # Case 2: include i
-        cover.add(i)
-        dfs(i + 1, cover)
-        cover.remove(i)
+                        valid = False
+                        break
+                if not valid:
+                    break
 
-    dfs(0, set())
-    return best
+            if valid:
+                return cover
+
+    return None
